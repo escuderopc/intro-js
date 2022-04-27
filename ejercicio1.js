dba97210868612f0eee61c8a48542571592fe651
escuderopc <escuderop.carina@gmail.com>
2022-04-26 21:37:19 -0500

    Actividad 1 Kata JavaScript

---
+++ ejercicio1.js
@@ -0,0 +1,9 @@
+//El usuario escribe un numero y despues este se multiplica por 2
+var numeroUsuario = prompt("Escribe un número: ")
+var numeroMul = 2
+var multiplicacion = numeroUsuario*numeroMul
+//Para mostar con alert
+alert("La suma es: "+ multiplicacion)
+//Para mostrar en la consola
+console.log("La suma es: ", multiplicacion )
+
---
+++ ejercicio4.js
@@ -0,0 +1,5 @@
+//Calcula el area de un rombo
+var ladoRombo_D = 8
+var ladoRombo_d = 6
+var areaRombo = (ladoRombo_D*ladoRombo_d)/2
+alert("El area del rombo es: "+areaRombo)
