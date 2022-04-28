a231a659041eee87ce1e58414300c0897dd1a5fc
escuderopc <escuderop.carina@gmail.com>
2022-04-27 23:02:56 -0500

    Actividad 1 Kata JavaScript

---
+++ Actividad 1/ejercicio2.js
@@ -0,0 +1,8 @@
+//Recibe dos numeros del usuario y los resta
+var numeroUsuario1 = prompt("Escribe el primer número: ")
+var numeroUsuario2 = prompt("Escribe el segundo número: ")
+var resta = numeroUsuario1 - numeroUsuario2;
+//Para mostar con alert
+alert("La resta es: "+resta)
+//Para mostrar en la consola
+console.log("La resta es: ", resta)
---
+++ Actividad 1/ejercicio3.js
@@ -0,0 +1,5 @@
+//Calcula el area de un circulo 
+const Pi = 3.1415
+var radio = 5.3
+var area = Pi * radio**2
+alert("El area del circulo con radio de 5.3 es: "+area)
