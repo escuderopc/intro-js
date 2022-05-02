f59136a62019df8c5ca8ecd57aa1bf131ee229cf
escuderopc <escuderop.carina@gmail.com>
2022-05-01 22:17:09 -0500

    Actividad 5 Kata JavaScript

---
+++ Actividad 5/ejercicio1.js
@@ -0,0 +1,7 @@
+//Se tiene un arreglo, iterar en un ciclo for para mostrar los valores que sean mayores a 3
+
+var array=[1, 4, 6, 10, 22, 55, 46, 2, 5, 0]
+console.log("Los valores mayores a 3 son: ")
+for(var i=0; i<=10; i++){
+    if(array[i]>=3)console.log(array[i])
+}
---
+++ Actividad 5/ejercicio2.js
@@ -0,0 +1,7 @@
+//Declarar un arreglo vacio con un ciclo while que se ejecute 5 veces, agregar los elementos al array
+var array = []
+var cont = 0
+while (cont<=4) {
+ array += prompt("Agrega un elemento al array ")
+ cont++
+}console.log("Los elementos del array son: "+array)
