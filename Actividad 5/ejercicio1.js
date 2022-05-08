67eaee9bdb07d51a0b39ff6d31c9eac789653593
escuderopc <escuderop.carina@gmail.com>
2022-05-08 12:52:28 -0500

    Actividad 5 Kata JavaScript

--- Actividad 5/ejercicio1.js
+++ Actividad 5/ejercicio1.js
@@ -2,6 +2,6 @@
 
 var array=[1, 4, 6, 10, 22, 55, 46, 2, 5, 0]
 console.log("Los valores mayores a 3 son: ")
-for(var i=0; i<=10; i++){
-    if(array[i]>=3)console.log(array[i])
+for(var i=0; i<array.length; i++){
+    if(array[i]>3)console.log(array[i])
 }
